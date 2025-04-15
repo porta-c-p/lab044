2bcafd55dfd90938964b81a2c814f5cb3c700452
Richard Chavez <richard.chavez@tecsup.edu.pe>
2025-04-15 14:50:40 -0500

    PrimeraActivity1

--- app/src/main/java/com/example/lab04app/PrimeraActivity.kt
+++ app/src/main/java/com/example/lab04app/PrimeraActivity.kt
@@ -11,6 +11,7 @@ class PrimeraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_primera)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
